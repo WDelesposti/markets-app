1a72bb56c695c8b57ef55df7f5795c17bbac51f8
WDelesposti <wallace.delesposti@gmail.com>
2023-10-11 23:12:35 -0300

    feat: install swagger in main

--- apps/market-service/src/main.ts
+++ apps/market-service/src/main.ts
@@ -1,23 +1,22 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+// src/main.ts
 
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
-import { AppModule } from './app/app.module';
+//import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { MarketsModule } from './markets/markets.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const app = await NestFactory.create(MarketsModule);
 
+  const config = new DocumentBuilder()
+    .setTitle('market-service')
+    .setDescription('The market-service API description')
+    .setVersion('0.1')
+    .build();
 
-  
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
-
 bootstrap();
