61040799b7088d16e732d7b9ae7c1bd03b01ddd2
WDelesposti <wallace.delesposti@gmail.com>
2023-12-02 22:17:22 -0300

    refactor: add trim and upperCase and conflict rules in product service

--- apps/market-service/src/services/product.service.ts
+++ apps/market-service/src/services/product.service.ts
@@ -1,26 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateProductDto } from '../dto/create-product.dto';
 import { UpdateProductDto } from '../dto/update-product.dto';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class ProductService {
-  constructor(private prisma: PrismaService){}
-  
-  create(createProductDto: CreateProductDto) {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createProductDto: CreateProductDto) {
     if (createProductDto.name === '') {
       throw new Error('Product name cannot be empty');
     }
+    createProductDto.name = createProductDto.name.trim().toUpperCase();
+
     if (createProductDto.brand === '') {
       throw new Error('Brand cannot be empty');
     }
+    createProductDto.brand = createProductDto.brand.trim().toUpperCase();
+
+    const exist = await this.findOneByNameAndBrand(
+      createProductDto.name,
+      createProductDto.brand,
+    );
+
+    if (exist.length > 0) {
+      throw new HttpException('Product already exist', HttpStatus.CONFLICT);
+    }
+
+    createProductDto.size = Number(createProductDto.size);
+    if (isNaN(createProductDto.size)) {
+      throw new Error('Size must be a number');
+    }
+
     if (createProductDto.size === 0) {
       throw new Error('Size cannot be zero');
     }
     if (createProductDto.measurement === '') {
       throw new Error('Measurement cannot be empty');
     }
-    return this.prisma.product.create({data: createProductDto});
+    createProductDto.measurement = createProductDto.measurement
+      .trim()
+      .toUpperCase();
+
+    return this.prisma.product.create({ data: createProductDto });
   }
 
   findAll() {
@@ -28,18 +50,32 @@ export class ProductService {
   }
 
   findOneByName(name: string) {
-    return this.prisma.product.findMany({where: {name: {contains: name, mode: 'insensitive'}}});
+    return this.prisma.product.findMany({
+      where: { name: { contains: name, mode: 'insensitive' } },
+    });
+  }
+
+  findOneByNameAndBrand(name: string, brand: string) {
+    return this.prisma.product.findMany({
+      where: {
+        name: { equals: name, mode: 'insensitive' },
+        brand: { equals: brand, mode: 'insensitive' },
+      },
+    });
   }
 
   findOne(id: number) {
-    return this.prisma.product.findUnique({where: {id}});
+    return this.prisma.product.findUnique({ where: { id } });
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
-    return this.prisma.product.update({where: {id}, data: updateProductDto});
+    return this.prisma.product.update({
+      where: { id },
+      data: updateProductDto,
+    });
   }
 
   remove(id: number) {
-    return this.prisma.product.delete({where: {id}});
+    return this.prisma.product.delete({ where: { id } });
   }
 }
