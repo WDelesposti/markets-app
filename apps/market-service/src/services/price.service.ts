cd06753ba6517aa3f94c1f2ad4ddd7bd5f650567
WDelesposti <wallace.delesposti@gmail.com>
2023-12-02 23:13:47 -0300

    chore: format

--- apps/market-service/src/services/price.service.ts
+++ apps/market-service/src/services/price.service.ts
@@ -5,10 +5,10 @@ import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class PriceService {
-  constructor(private prisma: PrismaService){}
-  
+  constructor(private prisma: PrismaService) {}
+
   create(createPriceDto: CreatePriceDto) {
-    return this.prisma.price.create({data: createPriceDto});
+    return this.prisma.price.create({ data: createPriceDto });
   }
 
   findAll() {
@@ -16,14 +16,14 @@ export class PriceService {
   }
 
   findOne(id: number) {
-    return this.prisma.price.findUnique({where: {id}});
+    return this.prisma.price.findUnique({ where: { id } });
   }
 
   update(id: number, updatePriceDto: UpdatePriceDto) {
-    return this.prisma.price.update({where: {id}, data: updatePriceDto});
+    return this.prisma.price.update({ where: { id }, data: updatePriceDto });
   }
 
   remove(id: number) {
-    return this.prisma.price.delete({where: {id}});
+    return this.prisma.price.delete({ where: { id } });
   }
 }
