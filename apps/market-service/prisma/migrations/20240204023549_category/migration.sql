84856af0f34cc7860d0e86ab73665b48d153b0ce
WDelesposti <wallace.delesposti@gmail.com>
2024-02-03 23:37:41 -0300

    feat: add category in product

---
+++ apps/market-service/prisma/migrations/20240204023549_category/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - Added the required column `categoryId` to the `Product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "categoryId" INTEGER NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
